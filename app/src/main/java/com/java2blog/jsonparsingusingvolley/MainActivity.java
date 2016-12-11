7a2d70594b488eab3d646b0bb9dc9710274e42f6
arpitmandliya <mandliya@gmail.com>
2016-12-11 12:05:07 +0530

    Updating web service URL

--- app/src/main/java/com/java2blog/jsonparsingusingvolley/MainActivity.java
+++ app/src/main/java/com/java2blog/jsonparsingusingvolley/MainActivity.java
@@ -33,8 +33,11 @@ public class MainActivity extends AppCompatActivity {
     private ProgressDialog progressDialog;
     ListView listView;
 
-    //Web services URL
-    private String jsonURl = "http://192.168.2.22:8080/JAXRSJsonExample/rest/countries";
+   // In case if you deploy rest web service, then use below link and replace below ip address with yours
+    //http://192.168.2.22:8080/JAXRSJsonExample/rest/countries
+    
+    //Direct Web services URL
+    private String path = "https://cdn.rawgit.com/arpitmandliya/AndroidRestJSONExample/master/countries.json";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
